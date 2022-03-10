c990c1c7dafdd8669ec0dc9f5f40dc0e26024cf7
Thomas Dokas <dokastho@umich.edu>
2022-03-09 22:56:46 -0500

    template started

--- snprocess/model.py
+++ snprocess/model.py
@@ -1,3 +1,4 @@
+import jinja2
 from qc.model import plink
 
 
@@ -5,3 +6,12 @@ def make_bed(inDir, inFile):
     """Convert SNP file to binary format. Return name of binary file."""
     inFileLink = inDir + inFile
     plink(" --file {} --make-bed --out {}merge".format(inFileLink, inDir))
+
+
+def md(output_path):
+    template_env = jinja2.Environment(
+        loader=jinja2.FileSystemLoader("templates"),
+        autoescape=jinja2.select_autoescape(['html', 'xml']), )
+    
+    temp = template_env.get_template("report_template.html")
+    output_path.write_text(temp.render("report.html"))
