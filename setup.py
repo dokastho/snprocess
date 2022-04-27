8ad9fc3f7fedad1f38280961f52b4942af5cbe4b
Thomas Dokas <dokastho@umich.edu>
2022-04-27 12:59:27 -0400

    updated setup and pypi version

--- setup.py
+++ setup.py
@@ -6,9 +6,17 @@ Thomas Dokas <dokastho@umich.edu>
 
 from setuptools import find_packages, setup
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='snprocess',
-    version='0.1.2',
+    version='0.1.3',
+    author="Thomas Dokas",
+    author_email="dokastho@umich.edu",
+    description="A SNP processing package",
+    url="https://github.com/dokastho/snprocess",
+    long_description=long_description,
     packages=find_packages(include=['snprocess', 'qc', 'templates'], exclude=['testout']),
     include_package_data=True,
     install_requires=[
@@ -21,6 +29,10 @@ setup(
         'matplotlib',
         'pandas',
         'jinja2',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: >3.6",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     entry_points={
