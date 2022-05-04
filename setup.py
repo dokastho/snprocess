5f6abd04a01383a15a31ddf9d0a887e937102659
Thomas Dokas <dokastho@umich.edu>
2022-05-04 15:57:38 -0400

    DOKFAMSERVER-2022.05.04_15.57.38-tmp2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='snprocess',
-    version='0.1.9',
+    version='0.2.0',
     author="Thomas Dokas",
     author_email="dokastho@umich.edu",
     description="A SNP processing package",
